574f7b81e8b88215df5ac4b012783df0fefea1f2
MR.SB <157207925+MRSB-01@users.noreply.github.com>
2025-07-19 23:50:12 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const AppConstant = {
     //BACKEND_URL: "http://localhost:8080/api/v1.0",
-    BACKEND_URL: "https://authify-spring-boot-backend.onrender.com/api/v1.0/"
+    BACKEND_URL: "https://authify-spring-boot-backend.onrender.com/api/v1.0"
 }
