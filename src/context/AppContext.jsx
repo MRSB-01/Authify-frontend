d661929807f5495ce4c2c85cb4da02402b17c008
MR.SB <157207925+MRSB-01@users.noreply.github.com>
2025-07-20 13:59:53 +0530

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -14,6 +14,7 @@ const AppContextProvider = (props) => {
     const getUserData = async () => {
         
     try {
+        const token = localStorage.getItem("token");
         const response = await axios.get(`${backendURL}/me`, {
   withCredentials: true,
   headers: {
