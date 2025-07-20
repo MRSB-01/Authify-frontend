3fa665f4518abd86cef66c515b925e03bf658899
MR.SB <157207925+MRSB-01@users.noreply.github.com>
2025-07-20 14:19:03 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://authify-spring-boot-backend.onrender.com/api/v1.0",
+const axiosInstance = axios.create({
+  baseURL: 'https://authify-spring-boot-backend.onrender.com/api/v1.0',
+  withCredentials: true,
 });
 
 instance.interceptors.request.use((config) => {
