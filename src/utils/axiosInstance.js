6850f18f018224d3a2e5affbcd21c193619fa80f
MR.SB <157207925+MRSB-01@users.noreply.github.com>
2025-07-21 19:41:21 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,6 +3,23 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: 'https://authify-spring-boot-backend.onrender.com/api/v1.0',
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+});
+
+// Add CSRF token interceptor if your backend uses CSRF protection
+instance.interceptors.request.use(config => {
+  const csrfToken = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('XSRF-TOKEN='))
+    ?.split('=')[1];
+  
+  if (csrfToken) {
+    config.headers['X-CSRF-TOKEN'] = csrfToken;
+  }
+  return config;
 });
 
 export default instance;
