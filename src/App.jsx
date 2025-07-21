6834c245c7857430e111012d46663258ee1a275f
MR.SB <157207925+MRSB-01@users.noreply.github.com>
2025-07-21 22:52:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { ToastContainer } from 'react-toastify'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import EmailVerify from './pages/EmailVerify'
-import ResetPassword from './pages/ResetPassword'
-import { useContext, useEffect } from 'react'
-import { AppContext } from './context/AppContext'
+import { ToastContainer } from 'react-toastify';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import EmailVerify from './pages/EmailVerify';
+import ResetPassword from './pages/ResetPassword';
+import { useContext, useEffect } from 'react';
+import { AppContext } from './context/AppContext';
 
 function App() {
-
   const { getUserData } = useContext(AppContext);
 
   useEffect(() => {
-    getUserData(); // ✅ Restore session on reload
-  }, []);
-
+    getUserData().catch((error) => console.error('Failed to get user data:', error));
+  }, [getUserData]);
 
   return (
     <>
-      <div>
-        <ToastContainer />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/email-verify' element={<EmailVerify />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-        </Routes>
-      </div>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
