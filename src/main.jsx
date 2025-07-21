a22a709fa78cc25cca232effd156fb4cced37d8a
MR.SB <157207925+MRSB-01@users.noreply.github.com>
2025-07-21 22:21:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,37 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
+import React from 'react'
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error) {
+    console.error('ErrorBoundary caught:', error)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div className="p-4 text-red-500">Something went wrong. Please refresh the page.</div>
+    }
+    return this.props.children
+  }
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>
-
+  <React.StrictMode>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </React.StrictMode>
 )
